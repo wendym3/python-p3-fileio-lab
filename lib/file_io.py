46783d097aef8df3c86c8197b0d677384c580b8b
wendymasese <wendy.masese@student.moringaschool.com>
2024-07-10 16:22:35 +0300

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', 'w') as f :
+        f.write (file_content)
     pass
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as file:
+        file.write(append_content)
+   
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as file:
+        return file.read()
